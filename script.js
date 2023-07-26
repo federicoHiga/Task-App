1862eff5112671ad3cd5df7973d4bf2c7f526e1c
fede <higamaradonafederico@gmail.com>
2023-07-26 18:35:23 -0300

    clase 04 completada

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
-// data- attributes
-// querySelector pero sin utilizar id o class, por si es necesario cambiarlos. El data- es un tag aun mas especifico para llamar desde js
+( ()=>{
 
 const btn = document.querySelector("[data-form-btn]");
 
-console.log(btn); //hacemos hablar al programa viendo el selector q linkeamos del html
+console.log(btn); 
 
-//listener 
-//evento para "escuchar" lo q sucede con el elemento linkeado, puede ser un click, un hover, arrastrar
-//al listener se le agrega el evento "click" y una funcion()
-
-// btn.addEventListener("click", (evento)=>{ //arrow function ()=>
-//     evento.preventDefault; //indicamos q el evento "evento" va a recetear el form para q no se autorefresque  
-//     const input = querySelector(["data-form-input"]); //la funcion captura el input
-//     console.log(input.value); //y muestra el .value 
-// });
-
-//pasado en limpio con la arrow function quedaria asi:
-//function
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    const value = input.value;//dejamos el valor capturado en una variable
-    const list = document.querySelector("[data-list]");//captura el elemento a crear
-    const task = document.createElement('li');//crea el elemnto
-    task.classList.add("card");//agrega el css por su class
-    input.value = "";//reset del form
-    //con backticks template podemos replicar estructura html e sumando variables JS como ${variable}
+    const value = input.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement('li');
+    task.classList.add("card");
+    input.value = "";
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerHTML = value;
+    taskContent.appendChild(titleTask);
     const content = 
-    `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    //con .innerHTML imprimimos la estuctura
-    task.innerHTML = content;
-
-    list.appendChild(task);//imprime 
+    `<i class="fas fa-trash-alt trashIcon icon"></i>`;
+    
+    // task.innerHTML = content;
+    task.appendChild(taskContent);
+    list.appendChild(task);
 
     console.log(task);
 }
 //call
 btn.addEventListener('click', createTask);
+
+//function para el check square. Al ser varias lineas de html es conveniente armar una funcion
+
+const checkComplete = ()=>{
+    const i = document.createElement("i");
+    i.classList.add("far","fa-check-square","icon");
+    i.addEventListener("click", completeTask);
+    return i;
+};
+
+//function para hacer dinamico el check square seleccionando y deseleccionando
+
+const completeTask = (event)=>{
+    const element = event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+};
+
+})();
